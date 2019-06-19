5ca9c7943864f07a769b0445d2a6d4a47cf9f569
alianver04 <alianver04@gmail.com>
2019-06-19 17:45:51 +0530

    web hook second try

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace testproject.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "successfully - first", "working with docker - try" };
+            return new string[] { "successfully - second", "working with docker - time" };
         }
 
         // GET api/values/5
