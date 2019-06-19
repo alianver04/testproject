0c2d4c924b9a3953ed2c0d01b23196b24987bc1c
alianver04 <51903585+alianver04@users.noreply.github.com>
2019-06-19 18:33:19 +0530

    Update ValuesController.cs

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace testproject.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "successfully - second", "working with docker - time" };
+            return new string[] { "successfully - second", "working in development - time" };
         }
 
         // GET api/values/5
